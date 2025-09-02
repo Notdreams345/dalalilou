bf240db3368f9f7b7f663d1a51e95a4546247505
Toni_4819 <132385090+Toni4819@users.noreply.github.com>
2025-09-02 11:26:29 +0200

    Update and rename helpers.js to package.js

--- scripts/helpers.js
+++
@@ -1,8 +0,0 @@
-function getProductById(id) {
-  return products.find(p => p.id === id);
-}
-
-function getQueryParam(key) {
-  const params = new URLSearchParams(window.location.search);
-  return params.get(key);
-}
---
+++ scripts/package.js
@@ -0,0 +1,18 @@
+// module exportant la liste des objets 3D
+export const packages = [
+  {
+    id: 'porte-clefs-dragon',
+    title: 'Porte-clés Dragon',
+    description: 'Miniature de dragon imprimée en PLA, 5 cm de haut.',
+    price: 8.90,
+    image: '/assets/images/dragon-keychain.jpg'
+  },
+  {
+    id: 'support-telephone',
+    title: 'Support Téléphone',
+    description: 'Support inclinable pour smartphone, design épuré.',
+    price: 12.50,
+    image: '/assets/images/phone-stand.jpg'
+  },
+  // ajoute autant d’objets que nécessaire
+];
